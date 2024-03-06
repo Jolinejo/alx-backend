62ede30cc46323bfe08515f7383e547a3af1304c
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2024-03-06 10:02:39 +0000

    task

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""queue cache
+"""
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """queue cache class
+    """
+
+    def __init__(self):
+        """intialize
+        """
+        super().__init__()
+        self.__queue = []
+
+    def put(self, key, item):
+        """insert
+        """
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.__queue.remove(key)
+            self.__queue.append(key)
+            self.cache_data[key] = item
+            return
+        self.__queue.append(key)
+        self.cache_data[key] = item
+        if len(self.cache_data) > super().MAX_ITEMS:
+            del self.cache_data[self.__queue[0]]
+            print("DISCARD: {}".format(self.__queue.pop(0)))
+
+    def get(self, key):
+        """retrieve
+        """
+        if key not in self.cache_data or key is None:
+            return None
+        self.__queue.remove(key)
+        self.__queue.append(key)
+        return self.cache_data.get(key)
