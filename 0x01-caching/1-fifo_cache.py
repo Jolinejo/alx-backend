c929de49903d04b069432e507a7672d2b3d10192
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2024-03-06 00:30:40 +0000

    task

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""queue cache"""
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """queue cache class"""
+
+    def __init__(self):
+        """intialize
+        """
+        super().__init__()
+        self.__queue = []
+
+    def put(self, key, item):
+        """insert
+        """
+        if key is None or item is None:
+            return
+        self.__queue.append(key)
+        self.cache_data[key] = item
+        if len(self.cache_data) > super().MAX_ITEMS:
+            del self.cache_data[self.__queue[0]]
+            print("DISCARD: {}".format(self.__queue.pop(0)))
+
+    def get(self, key):
+        """retrieve"""
+        if key not in self.cache_data or key is None:
+            return None
+        return self.cache_data.get(key)
