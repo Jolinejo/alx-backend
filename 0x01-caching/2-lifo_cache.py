eaaab22ed3cc9a50a900426d93c7a3e4f90b64f3
Mariam Ibrahim <117775223+Jolinejo@users.noreply.github.com>
2024-03-06 09:52:50 +0000

    task

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""stack cache
+"""
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """queue cache class
+    """
+
+    def __init__(self):
+        """intialize
+        """
+        super().__init__()
+        self.__stack = []
+
+    def put(self, key, item):
+        """insert
+        """
+        if key is None or item is None:
+            return
+        self.__stack.append(key)
+        self.cache_data[key] = item
+        if len(self.cache_data) > super().MAX_ITEMS:
+            del self.cache_data[self.__stack[-1]]
+            print("DISCARD: {}".format(self.__stack.pop()))
+
+    def get(self, key):
+        """retrieve
+        """
+        if key not in self.cache_data or key is None:
+            return None
+        return self.cache_data.get(key)
